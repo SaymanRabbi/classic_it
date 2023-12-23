775133caab02f6750a8e9c5f5f33a68f693518a6
sayman rabbi <mdrabbi.asm@gmail.com>
2023-12-23 16:10:18 +0600

    hero section

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from '../Hero/Hero';
 
 const ProductPage = () => {
     return (
         <div>
-            ProductPage
+           <Hero/>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import Navbar from './Components/Navbar/Navbar.jsx'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import NavbarContainer from './Components/Navbar/Navbar.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
