42b9cf94fd155df1865e6b71decf86d488633170
sayman rabbi <mdrabbi.asm@gmail.com>
2023-12-24 11:47:01 +0600

    changes logo url

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -50,7 +50,7 @@ const Login = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            src="../../../public/logo.png"
+            src="https://res.cloudinary.com/dnr5u3jpb/image/upload/v1703396445/logo_aqpnci.png"
             alt="Classic_it"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
